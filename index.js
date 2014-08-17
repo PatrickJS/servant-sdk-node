18eeebfc2cd9a03e28b34da0dd4ffdec2a8e1f1a
Austen Collins <austencollins@gmail.com>
2014-08-16 20:15:54 -0700

    Changes

--- index.js
+++ index.js
@@ -197,11 +197,21 @@ Servant.prototype._callAPI = function(http_method, uri, param_types, params, tok
 	});
 };
 
+
 /**
  * Instantiate Data Archetypes
  */
 
-console.log(jsonarchetypes);
+Servant.prototype.newArchetype = function(name) {
+	var name = name.toLowerCase();
+	if (typeof name !== 'string') throw new Error('Servant SDK Error - The newArchetype() method only accept a string for a name parameter');
+	if (jsonarchetypes[name] === 'undefined') throw new Error('Servant SDK Error - You have entered a JSON Archetype that does not exist: ' + name);
+	var instance = {};
+	for (property in jsonarchetypes[name].properties) {
+		instance[property] = jsonarchetypes[name].properties[property].default;
+	}
+	return instance;
+};
 
 
 module.exports = function(client_id, client_secret, redirect_uri, api_version) {
