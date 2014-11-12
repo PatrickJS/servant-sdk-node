e1df652e94db8db6d08c1fa75387337f45d2ed2f
Austen Collins <austencollins@gmail.com>
2014-11-12 15:36:32 -0800

    Remove array from property name

--- components/archetypes.js
+++ components/archetypes.js
@@ -263,10 +263,10 @@ _validateNestedArchetype = function(errors, rules, value) {
 _validateArray = function(errors, rules, array, property) {
     // Function to create array errors
     var createArrayError = function(errors, arrayproperty, objectproperty, index, err) {
-        if (!errors[arrayproperty + '_array']) errors[arrayproperty + '_array'] = {};
-        if (!objectproperty) return errors[arrayproperty + '_array'][index] = err;
-        if (!errors[arrayproperty + '_array'][index]) errors[arrayproperty + '_array'][index] = {};
-        return errors[arrayproperty + '_array'][index][objectproperty] = err;
+        if (!errors[arrayproperty]) errors[arrayproperty] = {};
+        if (!objectproperty) return errors[arrayproperty][index] = err;
+        if (!errors[arrayproperty][index]) errors[arrayproperty][index] = {};
+        return errors[arrayproperty][index][objectproperty] = err;
     };
     // Validate Array Root Schema
     var keys = Object.keys(rules);
