3c36a6b5a221fe489d27e3e1fdd4f7abf0913b14
Austen Collins <austencollins@gmail.com>
2014-11-18 18:19:08 -0800

    Fix authorization methods

--- components/authorization.js
+++ components/authorization.js
@@ -3,7 +3,7 @@
  */
 
 // Dependencies
-var unirest = require('unirest');
+var request = require('request');
 
 
 /**
@@ -12,28 +12,34 @@ var unirest = require('unirest');
 module.exports.exchangeAuthCode = function(ServantDefaults, authorization_code, callback) {
     // Check if User is authorized or not by which params have been received
     if (authorization_code) {
-        // User is UNAUTHORIZED, Fetch Refresh Token
+
         // Set Headers
         var headers = {
             'Connection': 'Keep-Alive',
-            'Host': 'www.servant.co',
-            'Accept': 'application/json',
+            'Host': 'api0.servant.co',
+            'Content-Type': 'application/json',
             'User-Agent': 'Servant Node SDK ' + ServantDefaults._sdk_version
         };
+
         // Set Data
         var data = JSON.stringify({
-            grant_type: 'authoriation_code',
+            grant_type: 'authorization_code',
             authorization_code: authorization_code,
             client_id: ServantDefaults._client_id,
-            client_secret: ServantDefaults._client_secret,
+            client_secret: ServantDefaults._client_secret
         });
-        var url = 'https://api0.servant.co/connect/oauth2/exchange_auth_code?grant_type=authorization_code&client_id=' + ServantDefaults._client_id + '&client_secret=' + ServantDefaults._client_secret + '&redirect_url=' + ServantDefaults._redirect_url + '&authorization_code=' + authorization_code;
+
         // Make Request to exchange AuthCode for AccessToken & Refresh Token
-        unirest.post(url).headers(headers).send(data).end(function(response) {
-            console.log(response);
-            // if (error) return callback(error, null);
-            // if (response.statusCode !== 200) return callback(JSON.parse(body), null);
-            // if (response.statusCode == 200) return callback(null, JSON.parse(body));
+        request({
+            headers: headers,
+            method: 'POST',
+            uri: 'https://api0.servant.co/connect/oauth2/exchange_auth_code',
+            body: data,
+            json: true
+        }, function(error, response, body) {
+            if (error) return callback(error, null);
+            if (response.statusCode !== 200) return callback(JSON.parse(body), null);
+            if (response.statusCode == 200) return callback(null, JSON.parse(body));
         });
     } else {
         throw new Error('Something has gone wrong with the authorization process.  Make sure the Connect URL is correct and it contains a response_type=code parameter.');
@@ -46,19 +52,31 @@ module.exports.exchangeAuthCode = function(ServantDefaults, authorization_code, 
  *  Refresh AccessToken via RefreshToken
  */
 module.exports.refreshAccessToken = function(ServantDefaults, refresh_token, callback) {
+    
     // Set Headers
     var headers = {
         'Connection': 'Keep-Alive',
-        'Host': 'www.servant.co',
+        'Host': 'api0.servant.co',
         'Content-Type': 'application/json',
         'User-Agent': 'Servant Node SDK ' + ServantDefaults._sdk_version
     };
+
+    // Set Data
+    var data = JSON.stringify({
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token,
+        authorization_code: authorization_code,
+        client_id: ServantDefaults._client_id,
+        client_secret: ServantDefaults._client_secret
+    });
+
     // Set Options for Request back to Servant
     var options = {
         method: 'GET',
         headers: headers
     };
-    options.url = 'https://api0.servant.co/connect/oauth2/refresh?grant_type=refresh_token&client_id=' + ServantDefaults._client_id + '&client_secret=' + ServantDefaults._client_secret + '&refresh_token=' + refresh_token;
+    options.url = 'https://api0.servant.co/connect/oauth2/refresh_access_token?';
+    
     // Make Request to exchange AuthCode for AccessToken & Refresh Token
     request(options, function(error, response, body) {
         if (error) return callback(error, null);
